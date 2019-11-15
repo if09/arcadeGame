5719541a09a69d196bcdeef58a1667c7f5126aec
Chillo <if@mmp.de>
2019-11-15 12:18:56 +0100

    Player Movement implemented

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 // Enemies our player must avoid
-let Enemy = function(y,x) {
+let Enemy = function (y, x) {
     this.x = x;
     this.y = y;
     this.speed = 0;
@@ -10,22 +10,22 @@ let Enemy = function(y,x) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 
-Enemy.prototype.randomSpeed = function(dt){
+Enemy.prototype.randomSpeed = function (dt) {
     let randomNum = Math.floor(Math.random() * (80 - 30 + 1)) + 30;
     this.speed = randomNum;
 }
 
-Enemy.prototype.update = function(dt) {
-   if(this.x > 550){
+Enemy.prototype.update = function (dt) {
+    if (this.x > 550) {
         this.randomSpeed();
         this.x = -50;
-   }
-    this.x += dt * this.speed; 
+    }
+    this.x += dt * this.speed;
     this.render();
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -33,41 +33,52 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-let Player = function(){
+let Player = function () {
     this.sprite = 'images/char-horn-girl.png';
     this.x = 200;
     this.y = 425;
 }
 
-Player.prototype.update = function(x, y){
+Player.prototype.update = function (x, y) {
     this.render(this.x, this.y)
 };
 
-Player.prototype.render = function(){
+Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function () {
-   
+Player.prototype.handleInput = function (allowedKeys) {
+    if (allowedKeys == "left" && this.x > 0) {
+        this.x -= 50;
+    }
+    if (allowedKeys == "right" && this.x < 400) {
+        this.x += 50;
+    }
+    if (allowedKeys == "down" && this.y < 425) {
+        this.y += 50;
+    }
+    if (allowedKeys == "up" && this.y > -25) {
+        this.y -= 50;
+    }
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let player  = new Player();
+let player = new Player();
 let enemy = new Enemy(65, -50);
 let enemy2 = new Enemy(140, -80);
 let enemy3 = new Enemy(225, -60);
 let allEnemies = [enemy, enemy2, enemy3];
 
 
-allEnemies.forEach(function(enemy){
+allEnemies.forEach(function (enemy) {
     enemy.randomSpeed();
 });
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
